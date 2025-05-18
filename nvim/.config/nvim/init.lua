9a981732c12573d133b4b8d9f49f3294fa7c9288
David Ryan <dryan@mbp.local>
2025-05-18 15:03:29 -0500

    fix

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -272,7 +272,7 @@ require("lazy").setup({
                 )
             end,
         },
-    }})
+    })
 
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
