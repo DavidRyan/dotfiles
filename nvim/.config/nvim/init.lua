cebdfb448b0bbe405af5e90869631384ad349172
david ryan <dryan31@gmail.com>
2025-10-17 13:49:09 -0500

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -124,8 +124,6 @@ require("lazy").setup({
             },
             lazy = false,
             config = function()
-                print("🔧 Setting up mason-lspconfig")
-
                 require("mason").setup()
 
                 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -286,7 +284,24 @@ require("lazy").setup({
         {
             "supermaven-inc/supermaven-nvim",
             config = function()
-                require("supermaven-nvim").setup({})
+                require("supermaven-nvim").setup({
+                    keymaps = {
+                        accept_suggestion = "<Tab>",
+                        clear_suggestion = "<C-]>",
+                        accept_word = "<C-j>",
+                    },
+                    ignore_filetypes = { cpp = true },
+                    color = {
+                        suggestion_color = "#ffffff",
+                        cterm = 244,
+                    },
+                    log_level = "info",
+                    disable_inline_completion = false,
+                    disable_keymaps = false,
+                    condition = function()
+                        return false
+                    end
+                })
             end,
         },
         {
@@ -304,25 +319,6 @@ require("lazy").setup({
 
     require("oil").setup()
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
-    require("supermaven-nvim").setup({
-        keymaps = {
-            accept_suggestion = "<Tab>",
-            clear_suggestion = "<C-]>",
-            accept_word = "<C-j>",
-        },
-        ignore_filetypes = { cpp = true }, -- or { "cpp", }
-        color = {
-            suggestion_color = "#ffffff",
-            cterm = 244,
-        },
-        log_level = "info", -- set to "off" to disable logging completely
-        disable_inline_completion = false, -- disables inline completion for use with cmp
-        disable_keymaps = false, -- disables built in keymaps for more manual control
-        condition = function()
-            return false
-        end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
-    })
 
 local lspconfig = require("lspconfig")
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -4,15 +4,18 @@ export COLORTERM=truecolor
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#555555'
 
-plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+#plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
 # Make sure syntax highlighting is sourced last for compatibility:
-source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+#source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 bindkey '^F' autosuggest-accept
 
 pokeget random --hide-name --mega-x --mega-y -s
 
 # opencode
 export PATH=/home/david/.opencode/bin:$PATH
+
+alias fastfetch='pokeget random --hide-name --mega-x --mega-y -s |
+fastfetch --file-raw -'
